f80859fcff84dc8944457714bba4b86cb31cfe23
Minh Hưng (Calvin) <118424791+MinhHung7@users.noreply.github.com>
2026-04-14 15:27:38 +0900

    Add solution for pandas-multi-column-selection

---
+++ study-plans/pandas-basics/pandas-multi-column-selection/pandas-multi-column-selection.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def select_columns(data, columns):
+    """
+    Returns: dict mapping selected column names to value lists
+    """
+    df = pd.DataFrame(data)
+
+    return df[columns].to_dict("list")
+    pass
